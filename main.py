eb0a8baab3ebb8a22dda73e901b4b72e277ea71b
Badara Diallo <badaradiallo@Host-005.lan>
2025-07-09 16:49:04 +0200

    Prepare Base Model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.cnnClassifier.pipeline.stage_01_data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.cnnClassifier.pipeline.stage_02_prepare_base_model_pipeline import PrepareBaseModelTraningPipeline
 from src.cnnClassifier import logger
 
 STAGE_NAME = "Data Ingestion stage"
@@ -10,3 +11,15 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+     
+
+STAGE_NAME = "Prepare base model"
+try:
+      logger.info(f"*******************")
+      logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+      obj = PrepareBaseModelTraningPipeline()
+      obj.initiate_prepare_base_model()
+      logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+      logger.exception(e)
+      raise e
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,10 +1,13 @@
+from pathlib import Path
 from src.cnnClassifier.utils.commons import create_directories,read_yaml
-from src.cnnClassifier.constant import CONFIG_FILE_PATH
-from src.cnnClassifier.entity.config_entity import DataIngestionConfig
+from src.cnnClassifier.constant import CONFIG_FILE_PATH,PARAMS_FILE_PATH
+from src.cnnClassifier.entity.config_entity import DataIngestionConfig,PrepareBaseModelConfig
 
 class ConfigurationManager:
-    def __init__(self,config_filepath=CONFIG_FILE_PATH):
+    def __init__(self,config_filepath=CONFIG_FILE_PATH,params_filepath=PARAMS_FILE_PATH):
         self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+        
         create_directories([self.config.artifacts_root])
         
     def get_data_ingestion_config(self)->DataIngestionConfig:
@@ -18,3 +21,21 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir 
         )
         return data_ingestion_config
+    
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
